5b3eacba6f08231c58d7f0669232df2acdadf5c1
bitorsic <bitorsic@gmail.com>
2024-09-08 01:31:29 +0530

    inited react project for frontend, register page built

---
+++ client/src/pages/Register.tsx
@@ -0,0 +1,145 @@
+import axios from "axios";
+import React, { useState } from "react"
+
+interface FormData {
+	email: string;
+	first_name: string;
+	last_name: string;
+	password: string;
+}
+
+export default function Register() {
+	const [formData, setFormData] = useState<FormData>({
+		email: "",
+		first_name: "",
+		last_name: "",
+		password: "",
+	});
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+		setFormData({
+			...formData,
+			[name]: value,
+		});
+	};
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		
+		try {
+			const response = await axios.post("http://localhost:3000/register", formData)
+			alert(response.data.message)
+		} catch (error: any){ // eslint-disable-line @typescript-eslint/no-explicit-any
+			alert(error.response.data.message)
+		}
+	};
+
+	return (
+		<section className="bg-gray-50 dark:bg-gray-900">
+			<div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+				<a
+					href="#"
+					className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+				>
+					Task-inator 3000
+				</a>
+				<div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+					<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+						<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+							Create an account
+						</h1>
+						<form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+							<div>
+								<label
+									htmlFor="email"
+									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+								>
+									Your email
+								</label>
+								<input
+									type="email"
+									name="email"
+									id="email"
+									value={formData.email}
+									onChange={handleChange}
+									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									placeholder="name@company.com"
+									required
+								/>
+							</div>
+							<div>
+								<label
+									htmlFor="first_name"
+									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+								>
+									First name
+								</label>
+								<input
+									type="text"
+									name="first_name"
+									id="first_name"
+									value={formData.first_name}
+									onChange={handleChange}
+									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									required
+								/>
+							</div>
+							<div>
+								<label
+									htmlFor="last_name"
+									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+								>
+									Last name
+								</label>
+								<input
+									type="text"
+									name="last_name"
+									id="last_name"
+									value={formData.last_name}
+									onChange={handleChange}
+									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									required
+								/>
+							</div>
+							<div>
+								<label
+									htmlFor="password"
+									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+								>
+									Password
+								</label>
+								<input
+									type="password"
+									name="password"
+									id="password"
+									placeholder="••••••••"
+									value={formData.password}
+									onChange={handleChange}
+									className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									required
+								/>
+							</div>
+							<button
+								type="submit"
+								className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+							>
+								Create an account
+							</button>
+							<p className="text-sm font-light text-gray-500 dark:text-gray-400">
+								Already have an account?{" "}
+								<a
+									href="#"
+									className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+								>
+									Login here
+								</a>
+							</p>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+
+	)
+}
--- server/routes/routes.go
+++ server/routes/routes.go
@@ -5,11 +5,13 @@ import (
 	"golang-backend/middleware"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func Setup(app *fiber.App) {
 	app.Use(logger.New())
+	app.Use(cors.New())
 
 	// auth
 	app.Post("/register", controllers.Register)
